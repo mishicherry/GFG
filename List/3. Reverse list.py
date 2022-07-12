bd76183e91b074661fe6d1a0fd4dde396ad5482f
Mishika Sahay <72877901+mishicherry@users.noreply.github.com>
2022-07-12 20:28:31 +0530

    Update 3. Reverse list.py

--- List/3. Reverse list.py
+++ List/3. Reverse list.py
@@ -4,7 +4,13 @@ Input : list = [10, 11, 12, 13, 14, 15]
 Output : [15, 14, 13, 12, 11, 10]
 """
 
-#
+#1st Mtd
 lst = [10, 11, 12, 13, 14, 15]
 arr = lst[::-1]
 print(arr)
+
+#2nd Mtd
+lst = [10, 11, 12, 13, 14, 15]
+
+for i in lst[::-1]:
+    print(i, end=" ")
